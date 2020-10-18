4af091d7028919f3520bae0de95b997ec10dd04e
Charles Jackson <cpjackson572@gmail.com>
2020-10-17 23:32:32 -0500

    done

--- prefix.py
+++ prefix.py
@@ -29,3 +29,10 @@ class Solution:
         """
         if not strs: return ""
         if len(strs) == 1: return strs[0]
+
+        strs.sort()
+        p = ""
+        for x, y in zip(strs[0], strs[-1]):
+            if x == y: p+=x
+            else: break
+        return r
